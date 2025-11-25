63b91bc549e2479abbd3705e66759b4400ee63d9
Salih Gökan Birdal <globalpayhub01@gmail.com>
2025-11-25 15:27:43 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
+import 'zone.js';
 import { provideHttpClient } from '@angular/common/http';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { SvgIconComponent, provideAngularSvgIcon } from 'angular-svg-icon';
+import { provideAngularSvgIcon } from 'angular-svg-icon';
 import { DemoAppComponent } from './app/demo-app.component';
 
 enableProdMode();
 
 bootstrapApplication(DemoAppComponent, {
     providers: [
-        importProvidersFrom(SvgIconComponent),
         provideAngularSvgIcon(),
         provideHttpClient()
     ]
